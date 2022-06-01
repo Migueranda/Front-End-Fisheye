bffc1a161f540793cc9b5527ddafac6f9d505d54
Miranda Pierre <migueranda.pierre@hotmail.com>
2022-06-01 08:48:36 +0200

    Début de la réalisation de la modal lightbox

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,16 +1,23 @@
-function photographerFactory(data) {
-    const { name, portrait } = data;
+function photographerFactory(data) {   
+    const { name, portrait, country, city, tagline, price, id} = data;    
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/photographers/${portrait} `;
+    
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
+       
+        const article = document.createElement( 'article' );  
+           //http://127.0.0.1:5500/Front-End-Fisheye/?id=930
+ 
+        let html = `<a href="photographer.html?id=${id}">
+                        <img src="${picture}" alt="${name}"/>
+                        <h2>${name}</h2>
+                    </a>             
+                    <h3>${city}, ${country}</h3>
+                    <h4>${tagline}</h4>
+                    <h5>${price}&euro;/jour</h5>`
+                
+        article.innerHTML = html;            
         return (article);
     }
     return { name, picture, getUserCardDOM }
